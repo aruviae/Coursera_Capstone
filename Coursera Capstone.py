fb14d39b1af60f06933501de925ae27b70515033
aruviae <73439248+aruviae@users.noreply.github.com>
2021-02-14 14:09:40 +0300

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1 align="center"><font size="5">IBM Data Science Capstone Project</font></h1>
+
+# The notebook will be used for a Capstone Project on IBM Data Science course.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
